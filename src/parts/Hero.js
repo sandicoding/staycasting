6de952b5d749c890e1e6f321b675520e2e78b459
unknown <sandicoding@gmail.com>
2020-07-25 09:56:55 +0700

    process create Hero

---
+++ src/parts/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import ImageHero from 'assets/images/img-hero.jpg'
+import ImageHero_ from 'assets/images/img-hero-frame.jpg'
+import IconCities from 'assets/images/icons/icon-cities.svg'
+import IconTraveler from 'assets/images/icons/icon-traveler.svg'
+import IconTreasure from 'assets/images/icons/icon-treasure.svg'
+
+export default function Hero(props) {
+    return (
+        <section className="container pt-4">
+            <div className="row align-items-center">
+                <div className="col-auto pr-5" style={{width}}>
+
+                </div>
+            </div>
+        </section>
+    )
+}
